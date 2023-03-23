3b6b8cde131f42b6b5d5197c413453f3df381c3b
diego_protec <diegosfprotec@gmail.com>
2023-03-23 19:39:21 -0300

    primeira configuração do projeto com todos os submodulos executando com sucesso

--- src/main/kotlin/com/contecin/api_gateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/contecin/api_gateway/ApiGatewayApplication.kt
@@ -2,10 +2,12 @@ package com.contecin.api_gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
-	runApplication<ApiGatewayApplication>(*args)
+    runApplication<ApiGatewayApplication>(*args)
 }
